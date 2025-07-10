748605b27f090382aca2435e2e823202aa5b74fb
jeromecagado <jerome.cagado@outlook.com>
2025-07-10 11:20:24 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignments1._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new();
+            student.StudentId = 007;
+            student.StudentFname = "Jerome";
+            student.StudentLname = "Cagado";
+            student.StudentGrade = 'A';
+
+            Console.WriteLine($"Student id: {student.StudentId}");
+            Console.WriteLine($"Student's name is: {student.StudentFname} {student.StudentLname}");
+            Console.WriteLine($"Student's grade is a: {student.StudentGrade}");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments1._4._2
+{
+    public class Student
+    {
+        private int studentId;
+        private string studentFname;
+        private string studentLname;
+        private char studentGrade;
+
+        public int StudentId { get; set; }
+        public string StudentFname { get; set; }
+        public string StudentLname { get; set; }
+        public char StudentGrade { get; set; }
+    }
+}
